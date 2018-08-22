b01cef7bee13c135d7976323a51c6e695765b52e
PyCharm <localhost>
2018-08-23 00:54:34 +0800

    init

---
+++ fileTemplates/internal/Setup Script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+$Import
+
+setup(
+    name='$Package_name',
+    version='$Version',
+    packages=$PackageList,$PackageDirs
+    url='$URL',
+    license='$License',
+    author='$Author',
+    author_email='$Author_Email',
+    description='$Description'
+)
